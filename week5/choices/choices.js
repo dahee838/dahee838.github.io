b5fb6aef396ff916567b6e45e9b6c6b6ae4fc77e
Dahee Lee <leed838@newschool.edu>
2018-03-06 21:37:19 -0500

    hnggg

--- week5/choices/choices.js
+++ week5/choices/choices.js
@@ -23,19 +23,19 @@ function draw() {
 
 function CreateScenesFromData(data) {
   for (var i = 0; i < data.length; i++) {
-    scenes.push(new Scene(data[i].sceneText, data[i].options, data[i].nextScenes, data[i].x,data[i].y,data[i].colorR,data[i].colorG,data[i].colorB,data[i].size))
+    scenes.push(new Scene(data[i].sceneText, data[i].options, data[i].nextScenes, data[i].x,data[i].y,data[i].colorR,data[i].colorG,data[i].colorB,))
   }
 }
 
 
-function Scene(sceneText, options, nextScenes, x,y, colorR,colorG,colorB, size) {
+function Scene(sceneText, options, nextScenes, x,y, colorR,colorG,colorB) {
   this.sceneText = sceneText;
   this.options = options;
   this.nextScenes = nextScenes;
   this.colorR = colorR;
-  this.colorG = colorR;
-  this.colorB = colorR;
-  this.size = size;
+  this.colorG = colorG;
+  this.colorB = colorB;
+  // this.size = size;
   this.x = x;
   this.y = y;
 
@@ -46,13 +46,11 @@ function Scene(sceneText, options, nextScenes, x,y, colorR,colorG,colorB, size) 
     textSize(42);
     text(this.sceneText, 100, 100);
 
-
-
     for (var i = 0; i < options.length; i++) {
       text('OPTION ' + (i + 1) + ': ' + this.options[i], 150, 200 + i * 50);
       noStroke();
       fill(this.colorR,this.colorG,this.colorB);
-      ellipse(this.x, this.y, this.size,this.size);
+      ellipse(this.x, this.y, 60,60);
     }
   }
 }
